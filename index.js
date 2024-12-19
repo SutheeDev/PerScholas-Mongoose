75a160db7688e58655f60b945e33da12f0b83917
SU-T <guitaristpeet@gmail.com>
2024-12-19 10:38:45 -0500

    listen to the port

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port : ${port}`);
+});
