9768e26bb5d6b6762f061db8f6e43c7541e99c03
SU-T <guitaristpeet@gmail.com>
2024-12-19 14:07:25 -0500

    populate seed data to the database

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
-app.use(express.json());
-
 const port = process.env.PORT || 5050;
+const conn = require("./config/db");
+conn();
+const starterFruits = require("./config/seed");
+const Fruit = require("./models/fruit");
+
+// home route
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// Seed route -> Populate our database with starter data
+app.get("/fruits/seed", async (req, res) => {
+  try {
+    await Fruit.deleteMany({});
+    await Fruit.create(starterFruits);
+    res.json(starterFruits);
+  } catch (error) {
+    console.log(`Something went wrong : ${error.message}`);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port : ${port}`);
